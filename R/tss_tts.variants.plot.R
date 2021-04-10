cdf1ed540ab0183d2a4086f5f1b72087e83c0014
Grey Monroe <greymonroe@gmail.com>
2021-04-10 12:48:51 -0700

    Update tss_tts.variants.plot.R

--- R/tss_tts.variants.plot.R
+++ R/tss_tts.variants.plot.R
@@ -6,17 +6,17 @@
 #' @import data.table
 #' @import ggplot2
 #' @export
-
 tss_tts.variants.plot<-function(positions, window=10, color="green4"){
   
   positions$bins<-as.numeric(as.character(cut(positions$pos, breaks=seq(-3000, 3000, by=window), labels=seq(-3000, 3000, by=window)[-1])))
-  positions_means<-positions[,.N, by=.(bins, loc)]
+  positions_means<-positions[!is.na(bins),.N, by=.(bins, loc)]
   plot<-ggplot(positions_means, aes(x=bins, y=N))+
-    geom_line(group=1, col=color)+
-    facet_grid(~loc)+
+    geom_line(aes(group=1), col=color, size=0.25)+
+    facet_grid(~loc, scales = "free")+
     theme_classic(base_size = 6)+
     scale_x_continuous(name="Relative genomic positions")+
-    geom_vline(xintercept=0, linetype="dashed", size=0.5)
+    geom_vline(xintercept=0, linetype="dashed", size=0.25)+
+    scale_y_continuous(name="Polymorphisms")
   return(plot)
 
 }
