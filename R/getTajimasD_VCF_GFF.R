f3d0d6bfa2832641788c1a862575738bbdb50d96
Grey Monroe <greymonroe@Monroe-LM.local>
2021-04-13 15:11:57 -0700

    update get

--- R/getTajimasD_VCF_GFF.R
+++ R/getTajimasD_VCF_GFF.R
@@ -1,13 +1,11 @@
-
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
 
 # when we run this we give the argument vcf which is the VCF file we want to analyze
 # and gff which is the gff file for example:
 # Rscript getTajimasD_for_gff.R TAIR10.gff 1001G.vcf.gz
-vcf<-args[1]
-gff<-read.table(args[2])
 
-getTajimasD(vcf, gff)
+library(polymorphology)
+getTajimasD(vcf=args[1], gff=args[2])
 
 
